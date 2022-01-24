6b44d384b828b2d999fc40bcf9d5569f92b52960
Guillermo Gomez Savino <ggomezsa@redhat.com>
2022-01-24 17:33:34 +0100

    Cleaned up the sources

--- fstab.sh
+++ fstab.sh
@@ -1,37 +1,6 @@
 #!/usr/bin/bash
-#set -o nounset
-#set -o errexit
-#set -o xtrace
 
-# Input yaml
-
-# Output fstab file on stdout
-
-# fstab file format (man fstab)
-# 1       2       3          4         5       6
-# fs_spec fs_file fs_vfstype fs_mntops fs_freq fs_passno
-
-# sample input
-#---
-#fstab:
-#  /dev/sda1:
-#    mount: /boot
-#    type: xfs
-#  /dev/sda2:
-#    mount: /
-#    type: ext4
-#  /deb/sdb1:
-#    mount: /var/lib/postgresql
-#    type: ext4
-#    root-reserve: 10%
-#  192.168.4..5:
-#    mount: /home
-#    export: /var/nfs/home
-#    type: nfs
-#    options:
-#      - noexec
-#      - nosuid
-
+# Simple /etc/fstab file generator from yaml formated data (fstab.yml)
 
 fstab_entries=$(yq e '.fstab|keys' fstab.yml -o csv | sed 's/,/ /g' | sed 's/\n//g')
 
@@ -39,12 +8,12 @@ for entry in $fstab_entries
 do
   # NFS case needs to fix the fs_spec field and concatenate export value
   yq_path=".fstab[\"$entry\"][\"type\"]"
-  fs_vfstype=$(yq eval "$yq_path"  fstab.yml)
+  fs_vfstype=$(yq eval "$yq_path" fstab.yml)
 
   case $fs_vfstype in
   nfs)
     yq_path=".fstab[\"$entry\"][\"export\"]"
-    export=$(yq eval "$yq_path"  fstab.yml)
+    export=$(yq eval "$yq_path" fstab.yml)
     fs_spec="$entry:$export"
     ;;
 
@@ -55,7 +24,7 @@ do
 
   # Mount point extraction
   yq_path=".fstab[\"$entry\"][\"mount\"]"
-  fs_file=$(yq eval "$yq_path"  fstab.yml)
+  fs_file=$(yq eval "$yq_path" fstab.yml)
 
   yq_path=".fstab[\"$entry\"][\"options\"] // [\"defaults\"] "
   fs_mntops=$(yq eval "$yq_path" -o csv fstab.yml)
